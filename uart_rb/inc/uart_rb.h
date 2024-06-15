459ee63d8c02e6ddc89127bb5641556d6aed03bf
Tommaso Clini <tommasoclini@gmail.com>
2024-06-15 16:23:03 +0200

    functioning version and example

--- uart_rb/inc/uart_rb.h
+++ uart_rb/inc/uart_rb.h
@@ -1,3 +1,6 @@
+#ifndef UART_RB_H
+#define UART_RB_H
+
 #include <lwrb/lwrb.h>
 #include <usart.h>
 
@@ -28,20 +31,85 @@ typedef enum {
     UART_RB_OK = 0,
     UART_RB_FAIL,
     UART_RB_INVALID_STATE,
+    UART_RB_INVALID_ARG,
     UART_RB_RB_FAIL,
     UART_RB_DMA_FAIL,
     UART_RB_UART_FAIL
 } uart_rb_err_t;
 
-/* Callbacks to be called by user */
+#define UART_RB_GET_RX_RB(x) (&(x)->rx_rb)
+#define UART_RB_GET_TX_RB(x) (&(x)->tx_rb)
+
+/**
+ * @brief Callback function for advancing rx ring buffer.
+ *
+ * This function must be called by user when an uart rx event occurs
+ *
+ * @param uart_rb Pointer to the UART ring buffer instance.
+ * @param pos Pos given in the uart rx evt callback
+ *
+ * @return UART_RB_OK on success, or an error code on failure.
+ */
 uart_rb_err_t uart_rb_rx_evt_cb(uart_rb_t *uart_rb, uint16_t pos);
+/**
+ * @brief Callback function called when a transmission has completed.
+ *
+ * This function must be called by user when a tx complete event occurs.
+ *
+ * @param uart_rb Pointer to the UART ring buffer instance.
+ *
+ * @return UART_RB_OK on success, or an error code on failure.
+ */
 uart_rb_err_t uart_rb_tx_tc_cb(uart_rb_t *uart_rb);
 
+/**
+ * @brief Initializes the UART ring buffer instance.
+ *
+ * Initializes the UART ring buffer instance with the provided parameters.
+ *
+ * @param uart_rb Pointer to the UART ring buffer instance.
+ * @param huart Pointer to the UART handle.
+ * @param rx_rb_data Pointer to the RX ring buffer data.
+ * @param rx_rb_data_size Size of the RX ring buffer data.
+ * @param tx_rb_data Pointer to the TX ring buffer data.
+ * @param tx_rb_data_size Size of the TX ring buffer data.
+ *
+ * @return UART_RB_OK on success, or an error code on failure.
+ */
 uart_rb_err_t uart_rb_init(uart_rb_t *uart_rb,
                  UART_HandleTypeDef *huart,
                  uint8_t *rx_rb_data, lwrb_sz_t rx_rb_data_size,
                  uint8_t *tx_rb_data, lwrb_sz_t tx_rb_data_size);
+/**
+ * @brief Deinitializes the UART ring buffer instance.
+ *
+ * Deinitializes the UART ring buffer instance.
+ *
+ * @param uart_rb Pointer to the UART ring buffer instance.
+ *
+ * @return UART_RB_OK on success, or an error code on failure.
+ */
 uart_rb_err_t uart_rb_deinit(uart_rb_t *uart_rb);
 
+/**
+ * @brief Starts the UART ring buffer instance.
+ *
+ * Starts the UART ring buffer instance, enabling the reception of data.
+ *
+ * @param uart_rb Pointer to the UART ring buffer instance.
+ *
+ * @return UART_RB_OK on success, or an error code on failure.
+ */
 uart_rb_err_t uart_rb_start(uart_rb_t *uart_rb);
+/**
+ * @brief Stops the UART ring buffer instance.
+ *
+ * Stops the UART ring buffer instance, disabling the reception of data.
+ *
+ * @param uart_rb Pointer to the UART ring buffer instance.
+ *
+ * @return UART_RB_OK on success, or an error code on failure.
+ */
 uart_rb_err_t uart_rb_stop(uart_rb_t *uart_rb);
+
+#endif /* UART_RB_H */
--- uart_rb/src/uart_rb.c
+++ uart_rb/src/uart_rb.c
@@ -1,118 +1,213 @@
 #include <uart_rb.h>
 
+#define UART_RB_PARAM_CHECK(x) \
+	if (!(x))              \
+	return UART_RB_INVALID_ARG
+
+/**
+ * @brief Transfers data from the TX ring buffer to the UART TX DMA.
+ *
+ * This function checks if there is data available in the TX ring buffer and if so,
+ * it transfers it to the UART TX DMA; it must be called for every tx tc event or when user wants to send data.
+ *
+ * @param uart_rb Pointer to the UART ring buffer instance.
+ *
+ * @return UART_RB_OK on success, or an error code on failure.
+ */
 static uart_rb_err_t uart_rb_tx_transfer(uart_rb_t *uart_rb)
 {
-    if (uart_rb->tx_current_size == 0)
-    {
-        if ((uart_rb->tx_current_size = lwrb_get_linear_block_read_length(&uart_rb->tx_rb)) > 0){
-            HAL_UART_Transmit_DMA(uart_rb->huart, lwrb_get_linear_block_read_address(&uart_rb->tx_rb), uart_rb->tx_current_size);
-            lwrb_skip(&uart_rb->tx_rb, uart_rb->tx_current_size);
-        }
-    }
+	// Check if the UART ring buffer instance is valid
+	UART_RB_PARAM_CHECK(uart_rb);
 
-    return UART_RB_OK;
+	uint32_t primask = __get_PRIMASK();
+	__disable_irq();
+	// Check if there is data available in the TX ring buffer
+	if (uart_rb->tx_current_size == 0) {
+		// Get the length of the linear block of data available for reading from the TX ring buffer
+		uart_rb->tx_current_size =
+			lwrb_get_linear_block_read_length(&uart_rb->tx_rb);
+		if (uart_rb->tx_current_size > 0) {
+			// Transmit the data using the UART TX DMA
+			HAL_UART_Transmit_DMA(
+				uart_rb->huart,
+				lwrb_get_linear_block_read_address(
+					&uart_rb->tx_rb),
+				uart_rb->tx_current_size);
+
+			// Skip the transferred data in the TX ring buffer
+			lwrb_skip(&uart_rb->tx_rb, uart_rb->tx_current_size);
+		}
+	}
+	__set_PRIMASK(primask);
+
+	return UART_RB_OK;
 }
 
 uart_rb_err_t uart_rb_rx_evt_cb(uart_rb_t *uart_rb, uint16_t pos)
 {
-    lwrb_sz_t size = (pos > uart_rb->rx_last_pos) ? (pos - uart_rb->rx_last_pos) : (uart_rb->rx_rb_data_size + pos - uart_rb->rx_last_pos);
-    uart_rb->rx_last_pos = pos;
+	// Check if the UART ring buffer instance is valid
+	UART_RB_PARAM_CHECK(uart_rb);
 
-    lwrb_advance(&uart_rb->rx_rb, size);
+	// Check if pos has changed(to be able to handle idle and ht events on the same pos)
+	if (pos != uart_rb->rx_last_pos) {
+		// Calculate the size of the newly received data
+		lwrb_sz_t size = (pos > uart_rb->rx_last_pos) ?
+					 (pos - uart_rb->rx_last_pos) :
+					 (uart_rb->rx_rb_data_size + pos -
+					  uart_rb->rx_last_pos);
 
-    return UART_RB_OK;
+		// Update the last received position
+		uart_rb->rx_last_pos = pos;
+
+		// Advance the ring buffer's write pointer by the size of the received data
+		lwrb_advance(&uart_rb->rx_rb, size);
+	}
+
+	return UART_RB_OK;
 }
 
 uart_rb_err_t uart_rb_tx_tc_cb(uart_rb_t *uart_rb)
 {
-    uart_rb->tx_current_size = 0;
-    uart_rb_tx_transfer(uart_rb);
+	// Check if the UART ring buffer instance is valid
+	UART_RB_PARAM_CHECK(uart_rb);
 
-    return UART_RB_OK;
+	// Reset the current size of the transmission
+	uart_rb->tx_current_size = 0;
+
+	// Trigger a new transmission
+	uart_rb_tx_transfer(uart_rb);
+
+	return UART_RB_OK;
 }
 
-static void uart_rb_txrb_evt_fn(lwrb_t *buff, lwrb_evt_type_t evt, lwrb_sz_t bp){
-    switch (evt)
-    {
-    case LWRB_EVT_WRITE:
-        uart_rb_tx_transfer(lwrb_get_arg(buff));
-        break;
-    }
-}
-
-uart_rb_err_t uart_rb_init(uart_rb_t *uart_rb,
-                 UART_HandleTypeDef *huart,
-                 uint8_t *rx_rb_data, lwrb_sz_t rx_rb_data_size,
-                 uint8_t *tx_rb_data, lwrb_sz_t tx_rb_data_size)
+/**
+ * @brief Callback function called when the TX ring buffer is written.
+ *
+ * This function is called when the TX ring buffer is written. It triggers a new transmission if
+ * there is data available in the TX ring buffer.
+ *
+ * @param buff Pointer to the TX ring buffer instance.
+ * @param evt Event type.
+ * @param bp Buffer position.
+ */
+static void uart_rb_txrb_evt_fn(lwrb_t *buff, lwrb_evt_type_t evt, lwrb_sz_t bp)
 {
-    if (uart_rb->initialized)
-    {
-        return UART_RB_INVALID_STATE;
-    }
+	// Check if the event is a write event
+	if (evt == LWRB_EVT_WRITE) {
+		// Get the UART ring buffer instance from the buffer argument
+		uart_rb_t *uart_rb = lwrb_get_arg(buff);
 
-    uart_rb->rx_rb_data_size = rx_rb_data_size;
-    uart_rb->tx_rb_data_size = tx_rb_data_size;
+		// Trigger a new transmission if there is data available in the TX ring buffer
+		uart_rb_tx_transfer(uart_rb);
+	}
+}
 
-    uart_rb->rx_rb_data = rx_rb_data;
-    uart_rb->tx_rb_data = tx_rb_data;
+uart_rb_err_t uart_rb_init(uart_rb_t *uart_rb, UART_HandleTypeDef *huart,
+			   uint8_t *rx_rb_data, lwrb_sz_t rx_rb_data_size,
+			   uint8_t *tx_rb_data, lwrb_sz_t tx_rb_data_size)
+{
+	// Check if UART ring buffer instance, UART handle and data pointers are valid
+	UART_RB_PARAM_CHECK(uart_rb);
+	UART_RB_PARAM_CHECK(huart);
+	UART_RB_PARAM_CHECK(rx_rb_data);
+	UART_RB_PARAM_CHECK(tx_rb_data);
 
-    uart_rb->huart = huart;
+	// Check if already initialized
+	if (uart_rb->initialized) {
+		return UART_RB_INVALID_STATE;
+	}
 
-    /* Init ring buffers */
-    if (!lwrb_init(&uart_rb->rx_rb, uart_rb->rx_rb_data, uart_rb->rx_rb_data_size) || !lwrb_init(&uart_rb->tx_rb, uart_rb->tx_rb_data, uart_rb->tx_rb_data_size)){
-        return UART_RB_RB_FAIL;
-    }
+	// Set UART ring buffer data sizes
+	uart_rb->rx_rb_data_size = rx_rb_data_size;
+	uart_rb->tx_rb_data_size = tx_rb_data_size;
 
-    lwrb_set_arg(&uart_rb->rx_rb, uart_rb);
-    lwrb_set_arg(&uart_rb->tx_rb, uart_rb);
+	// Set UART ring buffer data pointers
+	uart_rb->rx_rb_data = rx_rb_data;
+	uart_rb->tx_rb_data = tx_rb_data;
 
-    lwrb_set_evt_fn(&uart_rb->tx_rb, uart_rb_txrb_evt_fn);
+	// Set UART handle
+	uart_rb->huart = huart;
 
-    uart_rb->rx_last_pos = 0;
-    uart_rb->tx_current_size = 0;
+	// Initialize the ring buffers
+	if (!lwrb_init(&uart_rb->rx_rb, uart_rb->rx_rb_data,
+		       uart_rb->rx_rb_data_size) ||
+	    !lwrb_init(&uart_rb->tx_rb, uart_rb->tx_rb_data,
+		       uart_rb->tx_rb_data_size)) {
+		return UART_RB_RB_FAIL;
+	}
 
-    uart_rb->initialized = true;
+	// Set ring buffer arguments for custom context
+	lwrb_set_arg(&uart_rb->rx_rb, uart_rb);
+	lwrb_set_arg(&uart_rb->tx_rb, uart_rb);
 
-    return UART_RB_OK;
+	// Set TX ring buffer event function to start transfer whenever data is written
+	lwrb_set_evt_fn(&uart_rb->tx_rb, uart_rb_txrb_evt_fn);
+
+	// Reset RX and TX buffer pointers
+	uart_rb->rx_last_pos = 0;
+	uart_rb->tx_current_size = 0;
+
+	// Set initialized flag
+	uart_rb->initialized = true;
+
+	return UART_RB_OK;
 }
 
 uart_rb_err_t uart_rb_deinit(uart_rb_t *uart_rb)
 {
-    if (!uart_rb->initialized || uart_rb->started) {
-        return UART_RB_INVALID_STATE;
-    }
+	// Check if UART ring buffer instance is valid
+	UART_RB_PARAM_CHECK(uart_rb);
 
-    lwrb_reset(&uart_rb->rx_rb);
-    lwrb_reset(&uart_rb->tx_rb);
+	// Check if initialized and not started
+	if (!uart_rb->initialized || uart_rb->started) {
+		return UART_RB_INVALID_STATE;
+	}
 
-    uart_rb->initialized = false;
+	// Reset the ring buffers
+	lwrb_reset(&uart_rb->rx_rb);
+	lwrb_reset(&uart_rb->tx_rb);
 
-    return UART_RB_OK;
+	// Reset initialized flag
+	uart_rb->initialized = false;
+
+	return UART_RB_OK;
 }
-
 
 uart_rb_err_t uart_rb_start(uart_rb_t *uart_rb)
 {
-    if (uart_rb->started || !uart_rb->initialized){
-        return UART_RB_INVALID_STATE;
-    }
+	// Check if UART ring buffer instance is valid
+	UART_RB_PARAM_CHECK(uart_rb);
 
-    HAL_UARTEx_ReceiveToIdle_DMA(uart_rb->huart, uart_rb->rx_rb_data, uart_rb->rx_rb_data_size);
+	// Check if already started or not initialized
+	if (uart_rb->started || !uart_rb->initialized) {
+		return UART_RB_INVALID_STATE;
+	}
 
-    uart_rb->started = true;
-    
-    return UART_RB_OK;
+	// Start reception of data using DMA, receiving until idle
+	HAL_UARTEx_ReceiveToIdle_DMA(uart_rb->huart, uart_rb->rx_rb_data,
+				     uart_rb->rx_rb_data_size);
+
+	// Set started flag
+	uart_rb->started = true;
+
+	return UART_RB_OK;
 }
 
 uart_rb_err_t uart_rb_stop(uart_rb_t *uart_rb)
 {
-    if (!uart_rb->started){
-        return UART_RB_INVALID_STATE;
-    }
+	// Check if UART ring buffer instance is valid
+	UART_RB_PARAM_CHECK(uart_rb);
 
-    HAL_UART_AbortReceive(uart_rb->huart);
+	// Check if already stopped or not started
+	if (!uart_rb->started) {
+		return UART_RB_INVALID_STATE;
+	}
 
-    uart_rb->started = false;
-    
-    return UART_RB_OK;
+	// Abort the current reception of data using DMA
+	HAL_UART_AbortReceive(uart_rb->huart);
+
+	// Set stopped flag
+	uart_rb->started = false;
+
+	return UART_RB_OK;
 }
